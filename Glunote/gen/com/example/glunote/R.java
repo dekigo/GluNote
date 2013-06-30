aeae3ec94cd7fb46e7d8bf5281f6d9cbf9f7d134
T.J. Callahan <callat2@gmail.com>
2013-06-29 20:57:13 -0400

    database starting to take shape, some UI things

--- Glunote/gen/com/example/glunote/R.java
+++ Glunote/gen/com/example/glunote/R.java
@@ -47,9 +47,11 @@ public final class R {
         public static final int Carb_Input=0x7f050003;
         public static final int Carbs_Page_Carbs=0x7f05000a;
         public static final int Carbs_Page_Meal=0x7f050009;
+        public static final int Enter=0x7f05000b;
         public static final int Test_Input=0x7f050002;
         public static final int Test_Page_Bolus=0x7f050006;
         public static final int Test_Page_Glucose=0x7f050005;
+        public static final int Total_Carbs=0x7f05000c;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050008;
