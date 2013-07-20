f3b2e18233f13033db12e70036cc7f6e7d2815ff
T.J. Callahan <callat2@gmail.com>
2013-07-20 04:43:25 -0400

    messing with constants

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/88/c0bd305206f100121414c5e80f3a7eaf
@@ -0,0 +1,10 @@
+package com.example.glunote;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class CarbsPageDB extends SQLiteOpenHelper {
+
+}
--- Glunote/src/com/example/glunote/CarbsPageDb.java
+++ Glunote/src/com/example/glunote/CarbsPageDb.java
@@ -6,5 +6,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class CarbsPageDB extends SQLiteOpenHelper {
-
+	
+	public static final String TABLE_CARBS = "carbstable";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_TIME = "entrytime";
+	public static final String COLUMN_MEAL = "meal";
+	public static final String COLUMN_CARBS = "carbs";
+	
+	public static final String DATABASE_NAME = "carbstable.db";
+	public static final int DATABASE_VERSION = 1;
 }
